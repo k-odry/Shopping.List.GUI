34f4b0166756184016d2d42218c001c52c98b272
k-odry <odrykar@gmail.com>
2020-08-26 04:53:34 +0100

    Initial commit

--- ShopList.py
+++ ShopList.py
@@ -7,7 +7,7 @@ import os
 frame = Tk()
 # frame.attributes("-alpha", 0.9) # ---> set opacity of frame
 frame.title('Shopping List - by Karoly Odry')
-frame.geometry('300x600+200+80')
+frame.geometry('324x600+200+80')
 frame.iconbitmap("C:\\Users\\k\\PycharmProjects\\untitled\\ShopList\\bb_32x32.ico")
 frame.resizable(width=False, height=False)
 
@@ -45,7 +45,7 @@ def add_entry(row=None):
     n = len(chb)
     lb = Checkbutton(frame, text=e, variable=n, fg='#141951', bg='#ffffff', font=font, onvalue=1, offvalue=0,
                      command=lambda key=n: del_item(key))
-    lb.grid(row=row, column=0, padx=5, pady=0, ipady=0, sticky=NW)
+    lb.grid(row=row, column=0, padx=5, pady=0.5, ipady=0, sticky=NW)
     chb.append(lb)  # ---> new
 
 
@@ -80,7 +80,7 @@ def open_list(row=None):
             lb = Checkbutton(frame, text=elem, variable=var[index], fg='#141951', bg='#ffffff', font=font, onvalue=1,
                              offvalue=0,
                              command=lambda key=index: del_item(key))
-            lb.grid(row=row, column=0, padx=5, pady=0, ipady=0, sticky=NW)
+            lb.grid(row=row, column=0, padx=5, pady=0.5, ipady=0, sticky=NW)
             chb_list.append(lb)
 
 
@@ -89,15 +89,15 @@ en = Entry(frame, textvariable=entext, fg='#141951', bg='#e6dcb8', bd=4, relief=
 en.insert(0, "Click to Enter Item")
 en.configure(state=DISABLED)
 on_click_id = en.bind('<Button-1>', on_click)
-en.grid(row=0, column=0, padx=6, pady=10)
+en.grid(row=0, column=0, ipadx=7, ipady=1, padx=7, pady=9)
 
 add_btn = Button(frame, text='+', bg='#e6dcb8', relief='ridge', font=font, command=add_entry)
-add_btn.grid(row=0, column=1, padx=6, pady=10)
+add_btn.grid(row=0, column=1, padx=7, pady=9)
 
 save_btn = Button(frame, text='Save', bg='#e6dcb8', relief='ridge', font=font, command=save_list)
-save_btn.grid(row=0, column=2, padx=6, pady=10)
+save_btn.grid(row=0, column=2, padx=7, pady=9)
 
 load_btn = Button(frame, text='Load', bg='#e6dcb8', relief='ridge', font=font, command=open_list)
-load_btn.grid(row=0, column=3, padx=6, pady=10)
+load_btn.grid(row=0, column=3, padx=7, pady=9)
 
 frame.mainloop()
